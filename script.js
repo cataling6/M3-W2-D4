49c3094e61d70796a0274a03d796085764c12224
Catalin Radoi <radoi.catalin88@gmail.com>
2023-11-24 20:02:59 +0100

    Up Risorse

---
+++ script.js
@@ -0,0 +1,74 @@
+const viaggi = [
+  {
+    prezzo: 500,
+    destinazione: "Parigi",
+    dataPartenza: "2023-01-15",
+    dataRitorno: "2023-01-20",
+    descrizione: "Esplora la città dell'amore e visita la Torre Eiffel.",
+  },
+  {
+    prezzo: 700,
+    destinazione: "New York",
+    dataPartenza: "2023-02-10",
+    dataRitorno: "2023-02-18",
+    descrizione: "Scopri la Grande Mela e visita Times Square e Central Park.",
+  },
+  {
+    prezzo: 400,
+    destinazione: "Barcellona",
+    dataPartenza: "2023-03-05",
+    dataRitorno: "2023-03-10",
+    descrizione: "Goditi il sole sulla spiaggia di Barceloneta e visita la Sagrada Familia.",
+  },
+  {
+    prezzo: 600,
+    destinazione: "Tokyo",
+    dataPartenza: "2023-04-15",
+    dataRitorno: "2023-04-22",
+    descrizione: "Esplora la cultura giapponese e prova deliziosi piatti sushi.",
+  },
+  {
+    prezzo: 450,
+    destinazione: "Roma",
+    dataPartenza: "2023-05-10",
+    dataRitorno: "2023-05-17",
+    descrizione: "Ammira il Colosseo e assapora la cucina italiana autentica.",
+  },
+
+  {
+    prezzo: 1450,
+    destinazione: "Havana",
+    dataPartenza: "2023-01-10",
+    dataRitorno: "2023-01-27",
+    descrizione: "Ammira il Colosseo e assapora la cucina italiana autentica.",
+  },
+  {
+    prezzo: 1200,
+    destinazione: "Puerto Rico",
+    dataPartenza: "2023-03-02",
+    dataRitorno: "2023-03-14",
+    descrizione: "Ammira il Colosseo e assapora la cucina italiana autentica.",
+  },
+  {
+    prezzo: 650,
+    destinazione: "Bucarest",
+    dataPartenza: "2023-08-10",
+    dataRitorno: "2023-08-17",
+    descrizione: "Ammira il Colosseo e assapora la cucina italiana autentica.",
+  },
+
+  {
+    prezzo: 1000,
+    destinazione: "Amsterdam",
+    dataPartenza: "2023-08-10",
+    dataRitorno: "2023-08-27",
+    descrizione: "Ammira il Colosseo e assapora la cucina italiana autentica.",
+  },
+  {
+    prezzo: 10000,
+    destinazione: "Copparo",
+    dataPartenza: "2023-06-02",
+    dataRitorno: "2023-06-14",
+    descrizione: "Venite a trovarmi",
+  },
+];
